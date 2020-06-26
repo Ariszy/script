1dad6800edf8d1ae34940a9e8f5eaa94e1bbb22d
刘小康 <lxk@mchuan.com>
2020-06-26 20:24:40 +0800

    摇钱树测试

--- jd_moneyTree.js
+++ jd_moneyTree.js
@@ -103,7 +103,10 @@ function* step() {
   console.log('用户信息', userInfo);
 }
 function user_info() {
-  const data = '{"shareType":1,"source":0,"riskDeviceParam":"{\\\\"fp\\\\":\\\\"\\\\",\\\\"eid\\\\":\\\\"\\\\",\\\\"sdkToken\\\\":\\\\"\\\\",\\\\"sid\\\\":\\\\"\\\\"}"}'
+  // const data = '{"shareType":1,"source":0,"riskDeviceParam":"{\\\\"fp\\\\":\\\\"\\\\",\\\\"eid\\\\":\\\\"\\\\",\\\\"sdkToken\\\\":\\\\"\\\\",\\\\"sid\\\\":\\\\"\\\\"}"}'
+  const data = {
+    'reqData': '{"shareType":1,"source":0,"riskDeviceParam":"{\\\\"fp\\\\":\\\\"\\\\",\\\\"eid\\\\":\\\\"\\\\",\\\\"sdkToken\\\\":\\\\"\\\\",\\\\"sid\\\\":\\\\"\\\\"}"}'
+  }
   request('login', data);
 }
 
@@ -116,14 +119,41 @@ function sleep(response) {
     Task.next(response)
   }, 2000);
 }
+// function request(function_id, body = {}) {
+//   $hammer.request('GET', taskurl(function_id, body), (error, response) => {
+//     error ? $hammer.log("Error:", error) : sleep(JSON.parse(response.body));
+//   })
+// }
+// function taskurl(function_id, body = {}) {
+//   return {
+//     url: `${JD_API_HOST}/${function_id}?_=${new Date().getTime()*1000}&reqData=${escape(JSON.stringify(body))}`,
+//     headers: {
+//       'Cookie': cookie,
+//       'Host': 'ms.jr.jd.com',
+//       'Accept': 'application/json',
+//       'Origin': 'https://uuj.jr.jd.com',
+//       'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
+//       'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+//       'Referer': 'https://uuj.jr.jd.com/wxgrowing/moneytree7/index.html?channellv=sy',
+//       'Accept-Language': 'zh-CN,en-US;q=0.8',
+//       'X-Requested-With': 'com.jd.jrapp',
+//     }
+//   }
+// }
+
+
 function request(function_id, body = {}) {
-  $hammer.request('GET', taskurl(function_id, body), (error, response) => {
-    error ? $hammer.log("Error:", error) : sleep(JSON.parse(response.body));
+  $hammer.request('POST', taskurl(function_id, body), (error, response) => {
+    error ? $hammer.log("Error:", error) : sleep(JSON.parse(response));
   })
 }
-function taskurl(function_id, body = {}) {
+
+function taskurl(function_id, body) {
+  // console.log(`${JD_API_HOST}?functionId=${function_id}&body=${escape(JSON.stringify(body))}&appid=ld&client=apple&clientVersion=&networkType=&osVersion=&uuid=`)
   return {
-    url: `${JD_API_HOST}/${function_id}?_=${new Date().getTime()*1000}&reqData=${escape(JSON.stringify(body))}`,
+    // url: `${JD_API_HOST}?functionId=${function_id}&body=${escape(JSON.stringify(body))}&appid=ld&client=apple&clientVersion=&networkType=&osVersion=&uuid=`,
+    url: JD_API_HOST + '/' + function_id + '?_=' + new Date().getTime()*1000,
+    body: `${body}`,
     headers: {
       'Cookie': cookie,
       'Host': 'ms.jr.jd.com',
@@ -137,23 +167,3 @@ function taskurl(function_id, body = {}) {
     }
   }
 }
-
-
-// function request(function_id, body = {}) {
-//   $hammer.request('POST', taskurl(function_id, body), (error, response) => {
-//     error ? $hammer.log("Error:", error) : sleep(JSON.parse(response));
-//   })
-// }
-//
-// function taskurl(function_id, body) {
-//   // console.log(`${JD_API_HOST}?functionId=${function_id}&body=${escape(JSON.stringify(body))}&appid=ld&client=apple&clientVersion=&networkType=&osVersion=&uuid=`)
-//   return {
-//     // url: `${JD_API_HOST}?functionId=${function_id}&body=${escape(JSON.stringify(body))}&appid=ld&client=apple&clientVersion=&networkType=&osVersion=&uuid=`,
-//     url: JD_API_HOST + '/' + function_id + '?_=' + new Date().getTime()*1000,
-//     body: `${body}`,
-//     headers: {
-//       Cookie: cookie,
-//       UserAgent: `Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1`,
-//     }
-//   }
-// }
