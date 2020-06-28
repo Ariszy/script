c5e4cf073f77c4bcc042c87860eb44c9f116470d
刘小康 <lxk@mchuan.com>
2020-06-28 16:40:53 +0800

    add

---
+++ jd_speed.js
@@ -0,0 +1,148 @@
+const $hammer = (() => {
+  const isRequest = "undefined" != typeof $request,
+      isSurge = "undefined" != typeof $httpClient,
+      isQuanX = "undefined" != typeof $task;
+
+  const log = (...n) => { for (let i in n) console.log(n[i]) };
+  const alert = (title, body = "", subtitle = "", link = "") => {
+    if (isSurge) return $notification.post(title, subtitle, body, link);
+    if (isQuanX) return $notify(title, subtitle, (link && !body ? link : body));
+    log("==============📣系统通知📣==============");
+    log("title:", title, "subtitle:", subtitle, "body:", body, "link:", link);
+  };
+  const read = key => {
+    if (isSurge) return $persistentStore.read(key);
+    if (isQuanX) return $prefs.valueForKey(key);
+  };
+  const write = (val, key) => {
+    if (isSurge) return $persistentStore.write(val, key);
+    if (isQuanX) return $prefs.setValueForKey(val, key);
+  };
+  const request = (method, params, callback) => {
+    /**
+     *
+     * params(<object>): {url: <string>, headers: <object>, body: <string>} | <url string>
+     *
+     * callback(
+     *      error,
+     *      <response-body string>?,
+     *      {status: <int>, headers: <object>, body: <string>}?
+     * )
+     *
+     */
+    let options = {};
+    if (typeof params == "string") {
+      options.url = params;
+    } else {
+      options.url = params.url;
+      if (typeof params == "object") {
+        params.headers && (options.headers = params.headers);
+        params.body && (options.body = params.body);
+      }
+    }
+    method = method.toUpperCase();
+
+    const writeRequestErrorLog = function (m, u) {
+      return err => {
+        log("=== request error -s--");
+        log(`${m} ${u}`, err);
+        log("=== request error -e--");
+      };
+    }(method, options.url);
+
+    if (isSurge) {
+      const _runner = method == "GET" ? $httpClient.get : $httpClient.post;
+      return _runner(options, (error, response, body) => {
+        if (error == null || error == "") {
+          response.body = body;
+          callback("", body, response);
+        } else {
+          writeRequestErrorLog(error);
+          callback(error);
+        }
+      });
+    }
+    if (isQuanX) {
+      options.method = method;
+      $task.fetch(options).then(
+          response => {
+            response.status = response.statusCode;
+            delete response.statusCode;
+            callback("", response.body, response);
+          },
+          reason => {
+            writeRequestErrorLog(reason.error);
+            callback(reason.error);
+          }
+      );
+    }
+  };
+  const done = (value = {}) => {
+    if (isQuanX) return isRequest ? $done(value) : null;
+    if (isSurge) return isRequest ? $done(value) : $done();
+  };
+  return { isRequest, isSurge, isQuanX, log, alert, read, write, request, done };
+})();
+//直接用NobyDa的jd cookie
+const cookie = $hammer.read('CookieJD')
+const name = '天天加速';
+const JD_API_HOST = 'https://api.m.jd.com/';
+let Task = step();
+Task.next();
+
+let farmTask = null;
+// let farmInfo = null;
+
+function* step() {
+  if (!cookie) {
+    return $hammer.alert(name, '请先获取cookie\n直接使用NobyDa的京东签到获取');
+  }
+  const flyTask_state =  yield flyTask_state();
+  console.log(`flyTask_state的信息:${JSON.stringify(flyTask_state)}`);
+}
+function flyTask_start() {
+  const functionId = arguments.callee.name.toString();
+  const body = {
+    "source":"game",
+    "source_id":"{source_id}"
+  }
+  request(functionId, body)
+}
+function flyTask_state() {
+  const functionId = arguments.callee.name.toString();
+  const body = {
+    "source":"game",
+  }
+  request(functionId, body)
+}
+function request(function_id, body = {}) {
+  $hammer.request('GET', taskurl(function_id, body), (error, response) => {
+    console.log(response)
+    error ? $hammer.log("Error:", error) : sleep(JSON.parse(response.body));
+  })
+}
+
+function sleep(response) {
+  console.log('休息一下');
+  setTimeout(() => {
+    $hammer.log('休息结束');
+    // $hammer.log(response)
+    Task.next(response)
+  }, 2000);
+}
+
+function taskurl(function_id, body = {}) {
+  return {
+    url: `${JD_API_HOST}?functionId=${function_id}&appid=memberTaskCenter&body=${decodeURIComponent(JSON.stringify(body))}&jsonp=__jsonp1585235064779&_=1585235066058`,
+    headers: {
+      'Cookie': cookie,
+      'Host': 'api.m.jd.com',
+      'Accept': '*/*',
+      'Connection': 'keep-alive',
+      'User-Agent': 'jdapp;iPhone;8.5.5;13.4;9b812b59e055cd226fd60ebb5fd0981c4d0d235d;network/wifi;supportApplePay/3;hasUPPay/0;pushNoticeIsOpen/0;model/iPhone9,2;addressid/138109592;hasOCPay/0;appBuild/167121;supportBestPay/0;jdSupportDarkMode/0;pv/104.43;apprpd/MyJD_GameMain;ref/MyJdGameEnterPageController;psq/9;ads/;psn/9b812b59e055cd226fd60ebb5fd0981c4d0d235d|272;jdv/0|direct|-|none|-|1583449735697|1583796810;adk/;app_device/IOS;pap/JA2015_311210|8.5.5|IOS 13.4;Mozilla/5.0 (iPhone; CPU iPhone OS 13_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1',
+      'Accept-Language': 'zh-cn',
+      'Referer': 'https://h5.m.jd.com/babelDiy/Zeus/6yCQo2eDJPbyPXrC3eMCtMWZ9ey/index.html?lng=116.845095&lat=39.957701&sid=ea687233c5e7d226b30940ed7382c5cw&un_area=5_274_49707_49973',
+      'Accept-Encoding': 'gzip, deflate, br'
+    }
+  }
+}
