01b60499952935b7f12f9114a22fb5f20972e6e3
刘小康 <lxk@mchuan.com>
2020-07-08 15:29:58 +0800

    半自动提醒支付宝蚂蚁庄园喂食。

--- AlipayManor.js
+++ AlipayManor.js
@@ -1,10 +1,7 @@
-/*
-qx 及 loon 可用。
-半自动提醒支付宝蚂蚁庄园喂食。
-25 7 * * * AlipayManor.js
-
-自用 by lxk0301
-*/
+// qx 及 loon 可用。
+// 半自动提醒支付宝蚂蚁庄园喂食。
+// 15 */4 * * * AlipayManor.js
+// 自用 Modified from zZPiglet
 
 const $ = new cmp();
 const manor = "alipays://platformapi/startapp?appId=66666674"
