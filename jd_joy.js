681771a6d4146f9400142a30d905132b041c6599
刘小康 <lxk@mchuan.com>
2020-07-17 16:34:47 +0800

    改用chavy的Env.js兼容代码

--- jd_joy.js
+++ jd_joy.js
@@ -12,94 +12,7 @@ Combine from Zero-S1/JD_tools(https://github.com/Zero-S1/JD_tools)
 // Loon
 // [Script]
 // cron "15 */3 * * *" script-path=https://raw.githubusercontent.com/nzw9314/QuantumultX/master/Task/jd_joy.js,tag=京东宠汪汪
-const FEED_NUM = 10   // [10,20,40,80]
-
-const $hammer = (() => {
-    const isRequest = "undefined" != typeof $request,
-        isSurge = "undefined" != typeof $httpClient,
-        isQuanX = "undefined" != typeof $task;
-
-    const log = (...n) => { for (let i in n) console.log(n[i]) };
-    const alert = (title, body = "", subtitle = "", link = "") => {
-        if (isSurge) return $notification.post(title, subtitle, body, link);
-        if (isQuanX) return $notify(title, subtitle, (link && !body ? link : body));
-        log("==============📣系统通知📣==============");
-        log("title:", title, "subtitle:", subtitle, "body:", body, "link:", link);
-    };
-    const read = key => {
-        if (isSurge) return $persistentStore.read(key);
-        if (isQuanX) return $prefs.valueForKey(key);
-    };
-    const write = (val, key) => {
-        if (isSurge) return $persistentStore.write(val, key);
-        if (isQuanX) return $prefs.setValueForKey(val, key);
-    };
-    const request = (method, params, callback) => {
-        /**
-         *
-         * params(<object>): {url: <string>, headers: <object>, body: <string>} | <url string>
-         *
-         * callback(
-         *      error,
-         *      <response-body string>?,
-         *      {status: <int>, headers: <object>, body: <string>}?
-         * )
-         *
-         */
-        let options = {};
-        if (typeof params == "string") {
-            options.url = params;
-        } else {
-            options.url = params.url;
-            if (typeof params == "object") {
-                params.headers && (options.headers = params.headers);
-                params.body && (options.body = params.body);
-            }
-        }
-        method = method.toUpperCase();
-
-        const writeRequestErrorLog = function (m, u) {
-            return err => {
-                log("=== request error -s--");
-                log(`${m} ${u}`, err);
-                log("=== request error -e--");
-            };
-        }(method, options.url);
-
-        if (isSurge) {
-            const _runner = method == "GET" ? $httpClient.get : $httpClient.post;
-            return _runner(options, (error, response, body) => {
-                if (error == null || error == "") {
-                    response.body = body;
-                    callback("", body, response);
-                } else {
-                    writeRequestErrorLog(error);
-                    callback(error);
-                }
-            });
-        }
-        if (isQuanX) {
-            options.method = method;
-            $task.fetch(options).then(
-                response => {
-                    response.status = response.statusCode;
-                    delete response.statusCode;
-                    callback("", response.body, response);
-                },
-                reason => {
-                    writeRequestErrorLog(reason.error);
-                    callback(reason.error);
-                }
-            );
-        }
-    };
-    const done = (value = {}) => {
-        if (isQuanX) return isRequest ? $done(value) : null;
-        if (isSurge) return isRequest ? $done(value) : $done();
-    };
-    return { isRequest, isSurge, isQuanX, log, alert, read, write, request, done };
-})();
-
+const FEED_NUM = 10   //每次喂养数量 [10,20,40,80]
 const name = '京东宠汪汪';
 const $ = new Env(name);
 //直接用NobyDa的jd cookie
@@ -299,7 +212,7 @@ function sanVideo() {
   requestPost('https://draw.jdfcloud.com//pet/scan', body, 'application/json', 'weapp')
 }
 function request(url, reqSource) {
-    $.log("request url:", url);
+    console.log("request url:", url);
     const option =  {
         url: url,
         headers: {
@@ -320,7 +233,7 @@ function request(url, reqSource) {
 }
 
 function requestPost(url, body, ContentType, reqSource) {
-    $.log("request url:", url, "body:", body, "ContetentType:", ContentType);
+    console.log("request url:", url, "body:", body, "ContetentType:", ContentType);
     const options = {
         url: url,
         body: body,
