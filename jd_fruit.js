381f3f26a82cdc49ad4156c2a07de11a50563b7b
刘小康 <lxk@mchuan.com>
2020-07-18 15:17:00 +0800

    debug

--- jd_fruit.js
+++ jd_fruit.js
@@ -511,10 +511,6 @@ function* step() {
           message = '初始化农场数据异常, 请登录京东 app查看农场0元水果功能是否正常'
         }
     }
-    console.log('debug-----start')
-    console.log(jdNotify);
-    console.log(typeof (jdNotify));
-    console.log('debug-----end')
     if (!jdNotify || jdNotify === 'false') {
       $.msg(name, subTitle, message, option);
     }
