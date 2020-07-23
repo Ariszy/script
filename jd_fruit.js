38898887f0acaac22105f7765ccbf56ac8ad7be7
lxk0301 <lxk@mchuan.com>
2020-07-23 09:21:24 +0800

    格式化jd_fruit.js代码。2空格缩进

--- jd_fruit.js
+++ jd_fruit.js
@@ -17,7 +17,7 @@ cron "5 6-18/6 * * *" script-path=https://raw.githubusercontent.com/nzw9314/Quan
 
 let name = '东东农场';
 const $ = new Env(name);
-const Key = 'pt_key=AAJfC8e5ADAYEBHDmgPj1_S0lkRytXZkscP5qnStdwDj3yqldekb0Yu8e7PQ2bpPC57L4kNAh3Q;pt_pin=jd_704a2e5e28a66;';//单引号内自行填写您抓取的京东Cookie
+const Key = '';//单引号内自行填写您抓取的京东Cookie
 //直接用NobyDa的jd cookie
 const cookie = Key ? Key : $.getdata('CookieJD');
 //京东接口地址
