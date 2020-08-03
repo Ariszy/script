4bf742574d8619f4ad4242e4ec796eb2daa89416
lxk0301 <lxk@mchuan.com>
2020-08-03 15:07:23 +0800

    新增 取消关注店铺和取消关注商品脚本 因种豆得豆和宠汪汪脚本会关注店铺和商品，故此脚本用来取消已关注的店铺和商品

---
+++ jd_unsubscribe.js
@@ -0,0 +1,199 @@
+/*
+因种豆得豆和宠汪汪脚本会关注店铺和商品，故此脚本用来取消已关注的店铺和商品
+默认每运行一次脚本取消关注10个商品，10个店铺。可结合boxjs自定义取消多少个，先就这样子吧。
+// quantumultx
+[task_local]
+#取关京东店铺商品
+55 23 * * * https://gitee.com/lxk0301/scripts/raw/master/jd_unsubscribe.js, tag=取关京东店铺商品, enabled=true
+// Loon
+[Script]
+cron "55 23 * * *" script-path=https://gitee.com/lxk0301/scripts/raw/master/jd_unsubscribe.js,tag=取关京东店铺商品
+// Surge
+自己写，没有软件
+ */
+const $ = new Env('取消京东店铺及商品关注');
+const Key = 'pt_key=AAJfGoKPADCr1fEJpPMgWR3HviKGtCmd9NrzI4xd88aVkN28b1TxbH0urwb4J7Mjgw3MiepAVq0;pt_pin=jd_VTKxDTRDYRje;';//单引号内自行填写您抓取的京东Cookie
+const pageSize = $.getdata('jdUnsubscribePageSize') || 10;// 运行一次取消关注多少个商品及店铺。
+const jdNotify = $.getdata('jdUnsubscribeNotify');
+//直接用NobyDa的jd cookie
+const cookie = Key ? Key : $.getdata('CookieJD');
+const JD_API_HOST = 'https://wq.jd.com/fav';
+!(async () => {
+  if (!cookie) {
+    $.msg($.name, '【提示】请先获取cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/', {"open-url": "https://bean.m.jd.com/"});
+    return;
+  }
+  await unsubscribeShops();
+  await unsubscribeGoods();
+  await getFollowShops();
+  await getFollowGoods();
+  if (!jdNotify || jdNotify === 'false') {
+    $.msg($.name, '取消店铺及商品关注成功', `【已取消关注店铺】${$.unsubscribeShopsCount}个\n【已取消关注商品】${$.unsubscribeGoodsCount}个\n【还剩关注店铺】${$.shopsTotalNum}个\n【还剩关注商品】${$.goodsTotalNum}个\n`);
+  }
+})()
+    .catch((e) => {
+      $.log('', `❌ ${$.name}, 失败! 原因: ${e}!`, '')
+    })
+    .finally(() => {
+      $.done();
+    })
+function unsubscribeShops() {
+  return new Promise(async (resolve) => {
+    let followShops = await getFollowShops();
+    if (followShops.iRet === '0') {
+      let count = 0;
+      if (followShops.totalNum > 0) {
+        for (let item of followShops.data) {
+          let res = await unsubscribeShopsFun(item.shopId);
+          // console.log('取消关注店铺结果', res);
+          if (res.iRet === '0') {
+            console.log(`取消已关注店铺---${item.shopName}----成功\n`)
+            count ++;
+          } else {
+            console.log(`取消已关注店铺---${item.shopName}----失败\n`)
+          }
+        }
+        $.unsubscribeShopsCount = count;
+        resolve(count)
+      } else {
+        resolve(count)
+      }
+    }
+  })
+}
+function getFollowShops() {
+  return new Promise((resolve) => {
+    const option = {
+      url: `${JD_API_HOST}/shop/QueryShopFavList?cp=1&pageSize=${pageSize}&sceneval=2&g_login_type=1&callback=jsonpCBKA&g_ty=ls`,
+      headers: {
+        "Host": "wq.jd.com",
+        "Accept": "*/*",
+        "Connection": "keep-alive",
+        "Cookie": cookie,
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1",
+        "Accept-Language": "zh-cn",
+        "Referer": "https://wqs.jd.com/my/fav/shop_fav.shtml?sceneval=2&jxsid=15963530166144677970&ptag=7155.1.9",
+        "Accept-Encoding": "gzip, deflate, br"
+      },
+    }
+    $.get(option, (err, resp, data) => {
+      try {
+        data = JSON.parse(data.slice(14, -13));
+        $.shopsTotalNum = data.totalNum;
+      } catch (e) {
+        $.logErr(e, resp);
+      } finally {
+        resolve(data);
+      }
+    });
+  })
+}
+function unsubscribeShopsFun(shopId) {
+  return new Promise(resolve => {
+    const option = {
+      url: `${JD_API_HOST}/shop/batchunfollow?shopId=${shopId}&sceneval=2&g_login_type=1&callback=jsonpCBKG&g_ty=ls`,
+      headers: {
+        "Host": "wq.jd.com",
+        "Accept": "*/*",
+        "Connection": "keep-alive",
+        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+        'Referer': 'https://wqs.jd.com/my/fav/shop_fav.shtml?sceneval=2&jxsid=15960121319555534107&ptag=7155.1.9',
+        'Cookie': cookie,
+        "Accept-Language": "zh-cn",
+        "Accept-Encoding": "gzip, deflate, br"
+      },
+    }
+    $.get(option, (err, resp, data) => {
+      try {
+        data = JSON.parse(data.slice(14, -13));
+      } catch (e) {
+        $.logErr(e, resp);
+      } finally {
+        resolve(data);
+      }
+    });
+  })
+}
+function unsubscribeGoods() {
+  return new Promise(async (resolve) => {
+    let followGoods = await getFollowGoods();
+    if (followGoods.iRet === '0') {
+      let count = 0;
+      if (followGoods.totalNum > 0) {
+        for (let item of followGoods.data) {
+          let res = await unsubscribeGoodsFun(item.commId);
+          // console.log('取消关注商品结果', res);
+          if (res.iRet === '0') {
+            console.log(`取消关注商品---${item.commColor}---成功\n`)
+            count ++;
+          } else {
+            console.log(`取消关注商品---${item.commColor}---失败\n`)
+          }
+        }
+        $.unsubscribeGoodsCount = count;
+        resolve(count)
+      } else {
+        resolve(count)
+      }
+    }
+  })
+}
+function getFollowGoods() {
+  return new Promise((resolve) => {
+    const option = {
+      url: `${JD_API_HOST}/comm/FavCommQueryFilter?cp=1&pageSize=${pageSize}&category=0&promote=0&cutPrice=0&coupon=0&stock=0&areaNo=1_72_4139_0&sceneval=2&g_login_type=1&callback=jsonpCBKB&g_ty=ls`,
+      headers: {
+        "Host": "wq.jd.com",
+        "Accept": "*/*",
+        "Connection": "keep-alive",
+        "Cookie": cookie,
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1",
+        "Accept-Language": "zh-cn",
+        "Referer": "https://wqs.jd.com/my/fav/goods_fav.shtml?ptag=37146.4.1&sceneval=2&jxsid=15963530166144677970",
+        "Accept-Encoding": "gzip, deflate, br"
+      },
+    }
+    $.get(option, (err, resp, data) => {
+      try {
+        data = JSON.parse(data.slice(14, -13));
+        $.goodsTotalNum = data.totalNum;
+        // console.log('data', data.data.length)
+      } catch (e) {
+        $.logErr(e, resp);
+      } finally {
+        resolve(data);
+      }
+    });
+  })
+}
+function unsubscribeGoodsFun(commId) {
+  return new Promise(resolve => {
+    const option = {
+      url: `${JD_API_HOST}/comm/FavCommBatchDel?commId=${commId}&sceneval=2&g_login_type=1&callback=jsonpCBKM&g_ty=ls`,
+      headers: {
+        "Host": "wq.jd.com",
+        "Accept": "*/*",
+        "Connection": "keep-alive",
+        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+        'Referer': 'https://wqs.jd.com/my/fav/goods_fav.shtml?ptag=37146.4.1&sceneval=2&jxsid=15960121319555534107',
+        'Cookie': cookie,
+        "Accept-Language": "zh-cn",
+        "Accept-Encoding": "gzip, deflate, br"
+      },
+    }
+    $.get(option, (err, resp, data) => {
+      try {
+        data = JSON.parse(data.slice(14, -13).replace(',}', '}'));
+        // console.log('data', data);
+        // console.log('data', data.errMsg);
+      } catch (e) {
+        $.logErr(e, resp);
+      } finally {
+        resolve(data);
+      }
+    });
+  })
+}
+// prettier-ignore
+function Env(t,s){return new class{constructor(t,s){this.name=t,this.data=null,this.dataFile="box.dat",this.logs=[],this.logSeparator="\n",this.startTime=(new Date).getTime(),Object.assign(this,s),this.log("",`\ud83d\udd14${this.name}, \u5f00\u59cb!`)}isNode(){return"undefined"!=typeof module&&!!module.exports}isQuanX(){return"undefined"!=typeof $task}isSurge(){return"undefined"!=typeof $httpClient&&"undefined"==typeof $loon}isLoon(){return"undefined"!=typeof $loon}getScript(t){return new Promise(s=>{$.get({url:t},(t,e,i)=>s(i))})}runScript(t,s){return new Promise(e=>{let i=this.getdata("@chavy_boxjs_userCfgs.httpapi");i=i?i.replace(/\n/g,"").trim():i;let o=this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout");o=o?1*o:20,o=s&&s.timeout?s.timeout:o;const[h,a]=i.split("@"),r={url:`http://${a}/v1/scripting/evaluate`,body:{script_text:t,mock_type:"cron",timeout:o},headers:{"X-Key":h,Accept:"*/*"}};$.post(r,(t,s,i)=>e(i))}).catch(t=>this.logErr(t))}loaddata(){if(!this.isNode())return{};{this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),s=this.path.resolve(process.cwd(),this.dataFile),e=this.fs.existsSync(t),i=!e&&this.fs.existsSync(s);if(!e&&!i)return{};{const i=e?t:s;try{return JSON.parse(this.fs.readFileSync(i))}catch(t){return{}}}}}writedata(){if(this.isNode()){this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),s=this.path.resolve(process.cwd(),this.dataFile),e=this.fs.existsSync(t),i=!e&&this.fs.existsSync(s),o=JSON.stringify(this.data);e?this.fs.writeFileSync(t,o):i?this.fs.writeFileSync(s,o):this.fs.writeFileSync(t,o)}}lodash_get(t,s,e){const i=s.replace(/\[(\d+)\]/g,".$1").split(".");let o=t;for(const t of i)if(o=Object(o)[t],void 0===o)return e;return o}lodash_set(t,s,e){return Object(t)!==t?t:(Array.isArray(s)||(s=s.toString().match(/[^.[\]]+/g)||[]),s.slice(0,-1).reduce((t,e,i)=>Object(t[e])===t[e]?t[e]:t[e]=Math.abs(s[i+1])>>0==+s[i+1]?[]:{},t)[s[s.length-1]]=e,t)}getdata(t){let s=this.getval(t);if(/^@/.test(t)){const[,e,i]=/^@(.*?)\.(.*?)$/.exec(t),o=e?this.getval(e):"";if(o)try{const t=JSON.parse(o);s=t?this.lodash_get(t,i,""):s}catch(t){s=""}}return s}setdata(t,s){let e=!1;if(/^@/.test(s)){const[,i,o]=/^@(.*?)\.(.*?)$/.exec(s),h=this.getval(i),a=i?"null"===h?null:h||"{}":"{}";try{const s=JSON.parse(a);this.lodash_set(s,o,t),e=this.setval(JSON.stringify(s),i)}catch(s){const h={};this.lodash_set(h,o,t),e=this.setval(JSON.stringify(h),i)}}else e=$.setval(t,s);return e}getval(t){return this.isSurge()||this.isLoon()?$persistentStore.read(t):this.isQuanX()?$prefs.valueForKey(t):this.isNode()?(this.data=this.loaddata(),this.data[t]):this.data&&this.data[t]||null}setval(t,s){return this.isSurge()||this.isLoon()?$persistentStore.write(t,s):this.isQuanX()?$prefs.setValueForKey(t,s):this.isNode()?(this.data=this.loaddata(),this.data[s]=t,this.writedata(),!0):this.data&&this.data[s]||null}initGotEnv(t){this.got=this.got?this.got:require("got"),this.cktough=this.cktough?this.cktough:require("tough-cookie"),this.ckjar=this.ckjar?this.ckjar:new this.cktough.CookieJar,t&&(t.headers=t.headers?t.headers:{},void 0===t.headers.Cookie&&void 0===t.cookieJar&&(t.cookieJar=this.ckjar))}get(t,s=(()=>{})){t.headers&&(delete t.headers["Content-Type"],delete t.headers["Content-Length"]),this.isSurge()||this.isLoon()?$httpClient.get(t,(t,e,i)=>{!t&&e&&(e.body=i,e.statusCode=e.status),s(t,e,i)}):this.isQuanX()?$task.fetch(t).then(t=>{const{statusCode:e,statusCode:i,headers:o,body:h}=t;s(null,{status:e,statusCode:i,headers:o,body:h},h)},t=>s(t)):this.isNode()&&(this.initGotEnv(t),this.got(t).on("redirect",(t,s)=>{try{const e=t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString();this.ckjar.setCookieSync(e,null),s.cookieJar=this.ckjar}catch(t){this.logErr(t)}}).then(t=>{const{statusCode:e,statusCode:i,headers:o,body:h}=t;s(null,{status:e,statusCode:i,headers:o,body:h},h)},t=>s(t)))}post(t,s=(()=>{})){if(t.body&&t.headers&&!t.headers["Content-Type"]&&(t.headers["Content-Type"]="application/x-www-form-urlencoded"),delete t.headers["Content-Length"],this.isSurge()||this.isLoon())$httpClient.post(t,(t,e,i)=>{!t&&e&&(e.body=i,e.statusCode=e.status),s(t,e,i)});else if(this.isQuanX())t.method="POST",$task.fetch(t).then(t=>{const{statusCode:e,statusCode:i,headers:o,body:h}=t;s(null,{status:e,statusCode:i,headers:o,body:h},h)},t=>s(t));else if(this.isNode()){this.initGotEnv(t);const{url:e,...i}=t;this.got.post(e,i).then(t=>{const{statusCode:e,statusCode:i,headers:o,body:h}=t;s(null,{status:e,statusCode:i,headers:o,body:h},h)},t=>s(t))}}time(t){let s={"M+":(new Date).getMonth()+1,"d+":(new Date).getDate(),"H+":(new Date).getHours(),"m+":(new Date).getMinutes(),"s+":(new Date).getSeconds(),"q+":Math.floor(((new Date).getMonth()+3)/3),S:(new Date).getMilliseconds()};/(y+)/.test(t)&&(t=t.replace(RegExp.$1,((new Date).getFullYear()+"").substr(4-RegExp.$1.length)));for(let e in s)new RegExp("("+e+")").test(t)&&(t=t.replace(RegExp.$1,1==RegExp.$1.length?s[e]:("00"+s[e]).substr((""+s[e]).length)));return t}msg(s=t,e="",i="",o){const h=t=>!t||!this.isLoon()&&this.isSurge()?t:"string"==typeof t?this.isLoon()?t:this.isQuanX()?{"open-url":t}:void 0:"object"==typeof t&&(t["open-url"]||t["media-url"])?this.isLoon()?t["open-url"]:this.isQuanX()?t:void 0:void 0;this.isSurge()||this.isLoon()?$notification.post(s,e,i,h(o)):this.isQuanX()&&$notify(s,e,i,h(o)),this.logs.push("","==============\ud83d\udce3\u7cfb\u7edf\u901a\u77e5\ud83d\udce3=============="),this.logs.push(s),e&&this.logs.push(e),i&&this.logs.push(i)}log(...t){t.length>0?this.logs=[...this.logs,...t]:console.log(this.logs.join(this.logSeparator))}logErr(t,s){const e=!this.isSurge()&&!this.isQuanX()&&!this.isLoon();e?$.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t.stack):$.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t)}wait(t){return new Promise(s=>setTimeout(s,t))}done(t={}){const s=(new Date).getTime(),e=(s-this.startTime)/1e3;this.log("",`\ud83d\udd14${this.name}, \u7ed3\u675f! \ud83d\udd5b ${e} \u79d2`),this.log(),(this.isSurge()||this.isQuanX()||this.isLoon())&&$done(t)}}(t,s)}
+ 
