928b44dd29c166dea17881e341febfd7514eaa4c
lxk0301 <lxk@mchuan.com>
2020-08-15 18:48:40 +0800

    add

--- test2.js
+++ test2.js
@@ -1,3 +1,5 @@
 const jdCookieNode = require('./jdCookie.js');
-console.log('github action test', jdCookieNode)
-console.log('github action test')
+console.log('github action test', jdCookieNode);
+console.log('ACCESS_TOKEN', ${secrets.ACCESS_TOKEN});
+console.log('ACCESS_TOKEN', secrets.ACCESS_TOKEN);
+console.log('github action test');
