21ca0840a8a7a5c6e4455cd4f421ed4139e2c9b6
lxk0301 <778732665@qq.com>
2020-08-15 23:05:29 +0800

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,5 +1,5 @@
 const jdCookieNode = require('./jdCookie.js');
 console.log('github action test', jdCookieNode);
-console.log('ACCESS_TOKEN', process.env.ACCESS_TOKEN);
+console.log(process.env.ACCESS_TOKEN);
 console.log('JD_COOKIE', process.env.JD_COOKIE);
 console.log('github action test');
