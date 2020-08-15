c910c8de1736aa36002f2eab1e33a62b6f83aa82
lxk0301 <lxk@mchuan.com>
2020-08-15 15:26:04 +0800

    修复小问题

--- jd_unsubscribe.js
+++ jd_unsubscribe.js
@@ -1,6 +1,6 @@
 /*
 脚本：取关京东店铺和商品
-更新时间：2020-08-07
+更新时间：2020-08-15
 因种豆得豆和宠汪汪以及NobyDa大佬的京东签到脚本会关注店铺和商品，故此脚本用来取消已关注的店铺和商品
 默认每运行一次脚本取消关注10个商品，10个店铺。可结合boxjs自定义取消多少个（目前测试通过最大数量是一次性取消300个商品无异常，大于300请自行测试，建议尽量不要一次性全部取消以免出现问题）。
 建议此脚本运行时间在 种豆得豆和宠汪汪脚本运行之后 再执行
@@ -21,8 +21,8 @@ const $ = new Env('取关京东店铺和商品');
 const Key = '';
 //如需双账号签到,此处单引号内填写抓取的"账号2"Cookie, 否则请勿填写
 const DualKey = '';
-const goodPageSize = $.getdata('jdUnsubscribePageSize') || 10;// 运行一次取消多少个已关注的商品。
-const shopPageSize = $.getdata('jdUnsubscribeShopPageSize') || 10;// 运行一次取消多少个已关注的店铺。
+const goodPageSize = $.getdata('jdUnsubscribePageSize') || 10;// 运行一次取消多少个已关注的商品。数字0表示不取关任何商品
+const shopPageSize = $.getdata('jdUnsubscribeShopPageSize') || 10;// 运行一次取消多少个已关注的店铺。数字0表示不取关任何店铺
 const jdNotify = $.getdata('jdUnsubscribeNotify');
 const stop = $.getdata('jdUnsubscribeStopGoods') || '';//遇到此商品不再进行取关，此处内容需去商品详情页（自营处）长按拷贝商品信息
 const stopShop = $.getdata('jdUnsubscribeStopShop') || '';//遇到此店铺不再进行取关，此处内容请尽量从头开始输入店铺名称
@@ -72,7 +72,7 @@ function unsubscribeGoods(doubleKey) {
     if (followGoods.iRet === '0') {
       let count = 0;
       $.unsubscribeGoodsCount = count;
-      if (goodPageSize !== '0') {
+      if ((goodPageSize * 1) !== 0) {
         if (followGoods.totalNum > 0) {
           for (let item of followGoods.data) {
 
@@ -173,7 +173,7 @@ function unsubscribeShops() {
     if (followShops.iRet === '0') {
       let count = 0;
       $.unsubscribeShopsCount = count;
-      if (shopPageSize !== '0') {
+      if ((shopPageSize * 1) !== 0) {
         if (followShops.totalNum > 0) {
           for (let item of followShops.data) {
             if (stopShop && (item.shopName && item.shopName.indexOf(stopShop.replace(/\s*/g, '')) > -1)) {
