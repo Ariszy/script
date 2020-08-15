cb9f1781f0ad95c863193dc28c50384878bcb5b2
lxk0301 <lxk@mchuan.com>
2020-08-15 16:29:59 +0800

    添加jdCookie.js ，node.js用户可在此处填写ck。其他脚本都会引用此处ck

--- jdCookie.js
+++ jdCookie.js
@@ -3,5 +3,5 @@
 第6行代码处填写第一个京东账号ck
 第7行代码处填写第二个京东账号ck
  */
-exports.CookieJD = 'pt_key=AAJfCetdADCDahtAlLLr2xI7MFlBjr-sdRP_XlIX7hAH6VQXfQRQoLUaPBz0RTx5AVudSgg2hoI;pt_pin=jd_6cd93e613b0e5;';
-exports.CookieJD2 = 'pt_key=AAJfKpaxAEBdRN4DgKrh9lhxAp2lqzyZY2hipLPYCe-mISz4Nyq14IoNCVBXhUEhEuPVJ9D0KkfDjqln4heol2dt6cdt7efp;pt_pin=%E8%A2%AB%E6%8A%98%E5%8F%A0%E7%9A%84%E8%AE%B0%E5%BF%8633;';
+exports.CookieJD = '';
+exports.CookieJD2 = '';
