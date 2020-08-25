5393848958ca97194c8f0fabee2a88c109ce0636
lxk0301 <778732665@qq.com>
2020-08-25 16:17:25 +0800

    update

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -1,6 +1,7 @@
 /*
 种豆得豆 搬的https://github.com/uniqueque/QuantumultX/blob/4c1572d93d4d4f883f483f907120a75d925a693e/Script/jd_joy.js
-更新时间:2020-08-15
+更新时间:2020-08-25
+脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
 会自动关注任务中的店铺跟商品
 互助码shareCode请先手动运行脚本查看打印可看到
 // quantumultx
@@ -9,6 +10,8 @@
 // Loon
 [Script]
 cron "1 7-21/2 * * *" script-path=https://raw.githubusercontent.com/lxk0301/scripts/master/jd_plantBean.js,tag=京东种豆得豆
+// Surge
+// 京东种豆得豆 = type=cron,cronexp=1 7-21/2 * * *,wake-system=1,timeout=20,script-path=https://raw.githubusercontent.com/lxk0301/scripts/master/jd_joy_steal.js
 一天只能帮助3个人。多出的助力码无效
 注：如果使用Node.js, 需自行安装'crypto-js,got,http-server,tough-cookie'模块. 例: npm install crypto-js http-server tough-cookie got --save
 */
@@ -25,12 +28,10 @@ let jdNotify = $.getdata('jdPlantBeanNotify');
 //京东接口地址
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
 
-var plantUuids = [ // 这个列表填入你要助力的好友的plantUuid
-    'avlxbxdxf3altnm77gkqweriwik3gtnp3vhxdwy',
-    'olmijoxgmjutztzexoyxf22tw2cb5uw4ovuv4dq',
-    'qawf5ls3ucw25yhfulu32xekqy3h7wlwy7o5jii',
-    'zanmzshzq4ykx5xirwj7y7lmki',
-    'd6wg7f6syive54q4yfrdmaddo4'
+let plantUuids = [ // 这个列表填入你要助力的好友的plantUuid
+  '66j4yt3ebl5ierjljoszp7e4izzbzaqhi5k2unz2afwlyqsgnasq',
+  'olmijoxgmjutyrsovl2xalt2tbtfmg6sqldcb3q',
+  'qawf5ls3ucw25yhfulu32xekqy3h7wlwy7o5jii'
 ]
 let currentRoundId = null;//本期活动id
 let lastRoundId = null;//上期id
