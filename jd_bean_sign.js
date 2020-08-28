46ab91d92ae1db1c8d79a006a55d9ade4f89a0fb
lxk0301 <778732665@qq.com>
2020-08-28 16:27:31 +0800

    fix

--- jd_bean_sign.js
+++ jd_bean_sign.js
@@ -20,7 +20,7 @@ if ($.isNode()) {
     cookiesArr.push(jdCookieNode[item])
   })
 }
-!(async path => {
+!(async() => {
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/', {"open-url": "https://bean.m.jd.com/"});
     return;
