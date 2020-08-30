c9bd2a5fb623255caa3bc2673ae474a215b0d50b
lxk0301 <778732665@qq.com>
2020-08-30 21:00:07 +0800

    fix

--- jd_fruit.js
+++ jd_fruit.js
@@ -1054,7 +1054,7 @@ function requireConfig() {
     notify = $.isNode() ? require('./sendNotify') : '';
     //Node.js用户请在jdCookie.js处填写京东ck;
     const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
-    const jdFruitShareCodes = $.isNode() ?  require('./jdFruitShareCodes') : '';
+    const jdFruitShareCodes = $.isNode() ?  require('./jdFruitShareCodes.js') : '';
     //IOS等用户直接用NobyDa的jd cookie
     if ($.isNode()) {
       Object.keys(jdCookieNode).forEach((item) => {
