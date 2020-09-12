c08e42936c1e7a6f56499ceb4d45a31c33f2294e
lxk0301 <778732665@qq.com>
2020-09-12 13:33:02 +0800

    格式化代码

---
+++ jdPlantBeanShareCodes.js
@@ -0,0 +1,21 @@
+/*
+京东种豆得豆互助码
+此文件为Node.js专用。其他用户请忽略
+支持京东N个账号
+ */
+//云服务器腾讯云函数等NOde.js用户在此处填写东东萌宠的好友码。
+// github action用户的好友互助码填写到Action->Settings->Secrets->new Secret里面(Name填写 PLANT_BEAN_SHARECODES(此处的Name必须按此来写,不能随意更改),内容处填写互助码,填写规则如下)
+// 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间按Cookie隔开方法,即用&符号隔开,下面给一个示例
+// 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
+let PlantBeanShareCodes = [
+  '66j4yt3ebl5ierjljoszp7e4izzbzaqhi5k2unz2afwlyqsgnasq@olmijoxgmjutyrsovl2xalt2tbtfmg6sqldcb3q',//账号一的好友shareCode,不同好友中间用@符号隔开
+  '66j4yt3ebl5ierjljoszp7e4izzbzaqhi5k2unz2afwlyqsgnasq@olmijoxgmjutyrsovl2xalt2tbtfmg6sqldcb3q',//账号二的好友shareCode，不同好友中间用@符号隔开
+]
+// 判断github action里面是否有东东萌宠互助码
+if (process.env.PLANT_BEAN_SHARECODES && process.env.PLANT_BEAN_SHARECODES.split('&') && process.env.PLANT_BEAN_SHARECODES.split('&').length > 0) {
+  PlantBeanShareCodes = process.env.PLANT_BEAN_SHARECODES.split('&');
+}
+for (let i = 0; i < PlantBeanShareCodes.length; i++) {
+  const index = (i + 1 === 1) ? '' : (i + 1);
+  exports['PlantBeanShareCodes' + index] = PlantBeanShareCodes[i];
+}
