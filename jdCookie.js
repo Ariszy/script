0810c69d2ffa1f1bd3183418b600d236270ce4e7
lxk0301 <778732665@qq.com>
2020-09-19 14:50:04 +0800

    update

--- jdCookie.js
+++ jdCookie.js
@@ -3,12 +3,8 @@
  */
 //此处填写京东账号cookie。注：github action用户ck填写到Settings-Secrets里面
 let CookieJDs = [
-  'pt_key=AAJfWWZQADDNkv-rtrzqtzB1vi03MWtQ2lr9ftD066oAeyDpeFROOEhaFRN65qKd2nKZr_8Rafg;pt_pin=jd_6cd93e613b0e5;',//账号一ck
-  // 'pt_key=AAJfUizYAEC9DZlUpR_bmYOFNgwKXDMKoPX0rRDcXSWOaGemcfHTFDsJ3BkYOPcCTIprRm-OQfdoGQ2BEwxh7_qOnTEMUKHQ;pt_pin=%E8%A2%AB%E6%8A%98%E5%8F%A0%E7%9A%84%E8%AE%B0%E5%BF%8633;',//账号二ck,如有更多,依次类推
-  // 'pt_key=AAJfYrdqADAnCxWzDpJF_f5NsBXhKXIgJzjlkxlNHsDbGEATdXPhPUQ9_jrem_TSDcRVKAET1hc;pt_pin=zooooo58;',//账号二ck,如有更多,依次类推
-  // 'pt_key=AAJfPyXfADDBrCmpXn05CgKeO1b4DQpfpCgLyNv-1MM16HX0Otywuad7WxcnKBCAMdYujdr1JXs;pt_pin=jd_45a6b5953b15b;',//账号二ck,如有更多,依次类推
-  // 'pt_key=AAJfWw3pADAc-E_dD0LiU8RmNnaFquzTze7knrTV1v6bvsDUOJ9nTRXSwmSqLdg2z6lNofCEsHE;pt_pin=jd_704a2e5e28a66;',//账号二ck,如有更多,依次类推
-  // 'pt_key=AAJfR7GoADAYphNI7_i2CbzmZhfcZtP8sKXhzN9RBS_DYULdLoCSOA9bIAuHSO1tRzjcDj7SH5o;pt_pin=ecyduo;',//账号二ck,如有更多,依次类推
+  '',//账号一ck
+  '',//账号二ck,如有更多,依次类推
 ]
 // 判断github action里面是否有京东ck
 if (process.env.JD_COOKIE && process.env.JD_COOKIE.split('&') && process.env.JD_COOKIE.split('&').length > 0) {
