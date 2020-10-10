816531b7b05ced4c59c683f73dc587c0bbd2fd4d
lxk0301 <778732665@qq.com>
2020-10-10 14:40:40 +0800

    action云端互助码不同账号之间支持用换行隔开，兼容之前的&符号隔开

--- jdFruitShareCodes.js
+++ jdFruitShareCodes.js
@@ -5,15 +5,23 @@
  */
 //云服务器腾讯云函数等NOde.js用户在此处填写京东东农场的好友码。
 // github action用户的好友互助码填写到Action->Settings->Secrets->new Secret里面(Name填写 FruitShareCodes(此处的Name必须按此来写,不能随意更改),内容处填写互助码,填写规则如下)
-// 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间按Cookie隔开方法,即用&符号隔开,下面给一个示例
+// 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
 let FruitShareCodes = [
   '0a74407df5df4fa99672a037eec61f7e@dbb21614667246fabcfd9685b6f448f3@6fbd26cc27ac44d6a7fed34092453f77@61ff5c624949454aa88561f2cd721bf6',//账号一的好友shareCode,不同好友中间用@符号隔开
   '6fbd26cc27ac44d6a7fed34092453f77@61ff5c624949454aa88561f2cd721bf6',//账号二的好友shareCode，不同好友中间用@符号隔开
 ]
 // 判断github action里面是否有水果互助码
-if (process.env.FruitShareCodes && process.env.FruitShareCodes.split('&') && process.env.FruitShareCodes.split('&').length > 0) {
-  FruitShareCodes = process.env.FruitShareCodes.split('&');
+if (process.env.FruitShareCodes) {
+  if (process.env.FruitShareCodes.indexOf('&') > -1) {
+    console.log(`您的东东农场互助码选择的是用&隔开\n`)
+    FruitShareCodes = process.env.FruitShareCodes.split('&');
+  } else if (process.env.FruitShareCodes.indexOf('\n') > -1) {
+    console.log(`您的东东农场互助码选择的是用换行隔开\n`)
+    FruitShareCodes = process.env.FruitShareCodes.split('\n');
+  } else {
+    FruitShareCodes = process.env.FruitShareCodes.split();
+  }
 }
 for (let i = 0; i < FruitShareCodes.length; i++) {
   const index = (i + 1 === 1) ? '' : (i + 1);
