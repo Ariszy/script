402ff9d5949b8774c1c4060ed13030bc0615d474
lxk0301 <778732665@qq.com>
2020-10-10 14:18:15 +0800

    test

--- jdPetShareCodes.js
+++ jdPetShareCodes.js
@@ -5,15 +5,23 @@
  */
 //云服务器腾讯云函数等NOde.js用户在此处填写东东萌宠的好友码。
 // github action用户的好友互助码填写到Action->Settings->Secrets->new Secret里面(Name填写 PetShareCodes(此处的Name必须按此来写,不能随意更改),内容处填写互助码,填写规则如下)
-// 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间按Cookie隔开方法,即用&符号隔开,下面给一个示例
+// 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
 let PetShareCodes = [
   'MTAxODc2NTEzNTAwMDAwMDAwMjg3MDg2MA==@MTAxODc2NTEzMzAwMDAwMDAyNzUwMDA4MQ==@MTAxODc2NTEzMjAwMDAwMDAzMDI3MTMyOQ==@MTAxODc2NTEzNDAwMDAwMDAzMDI2MDI4MQ==',//账号一的好友shareCode,不同好友中间用@符号隔开
   'MTAxODc2NTEzMjAwMDAwMDAzMDI3MTMyOQ==@MTAxODcxOTI2NTAwMDAwMDAyNjA4ODQyMQ==',//账号二的好友shareCode，不同好友中间用@符号隔开
 ]
 // 判断github action里面是否有东东萌宠互助码
-if (process.env.PetShareCodes && process.env.PetShareCodes.split('&') && process.env.PetShareCodes.split('&').length > 0) {
-  PetShareCodes = process.env.PetShareCodes.split('&');
+if (process.env.PetShareCodes) {
+  if (process.env.PetShareCodes.indexOf('&') > -1) {
+    console.log(`您的东东萌宠互助码选择的是用&隔开\n`)
+    PetShareCodes = process.env.PetShareCodes.split('&');
+  } else if (process.env.PetShareCodes.indexOf('\n') > -1) {
+    console.log(`您的东东萌宠互助码选择的是用换行隔开\n`)
+    PetShareCodes = process.env.PetShareCodes.split('\n');
+  } else {
+    PetShareCodes = process.env.PetShareCodes.split();
+  }
 }
 for (let i = 0; i < PetShareCodes.length; i++) {
   const index = (i + 1 === 1) ? '' : (i + 1);
