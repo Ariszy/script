fc2d96334b3ef06b791b4fe9c1b1ed68d01ae186
ZhiYi-N <34622776+ZhiYi-N@users.noreply.github.com>
2020-11-27 19:38:07 +0800

    Update qqyd.js

--- qqyd.js
+++ qqyd.js
@@ -1,429 +1,97 @@
+const exec = require('child_process').execSync
+const fs = require('fs')
+const download = require('download')
+const smartReplace = require("../smartReplace");
 
+const $ = new Env('企鹅阅读');
+const notify = $.isNode() ? require('./sendNotify') : '';
+// 公共变量
 
+const Secrets = {
+    COOKIE_QQYD: process.env.COOKIE_QQYD,
+    PUSH_KEY: process.env.PUSH_KEY, //server酱推送消息
+    BARK_PUSH: process.env.BARK_PUSH, //Bark推送
+    TG_BOT_TOKEN: process.env.TG_BOT_TOKEN, //TGBot推送Token
+    TG_USER_ID: process.env.TG_USER_ID, //TGBot推送成员ID
+};
 
-
-resolve()
-
-    })
-
-   })
-  }  
-
-
-
-
-
-
-
-
-//用户名
-function qqreadinfo() {
-return new Promise((resolve, reject) => {
-
-  const toqqreadinfourl = {
-
-    url: qqreadurlVal,
-
-    headers: JSON.parse(qqreadheaderVal),
-    
- timeout:60000};
-   $.get(toqqreadinfourl,(error, response, data) =>{
-     if(logs) $.log(`${jsname}, 用户名: ${data}`)
-     info =JSON.parse(data)
-tz+=
-'【用户信息】:'+info.data.user.nickName+'\n'
-
-
-
-//阅读金币1
-function qqreadssr1() {
-return new Promise((resolve, reject) => {
-
-  const toqqreadssr1url = {url: `https://mqqapi.reader.qq.com/mqq/red_packet/user/read_time?seconds=30`,
-
-    headers: JSON.parse(qqreadheaderVal),
-   timeout:60000};
-
-if (config.data.pageParams.todayReadSeconds/60>=1){
-
-   $.get(toqqreadssr1url,(error, response, data) =>{
-     if(logs) $.log(`${jsname}, 金币奖励1: ${data}`)
-     ssr1 =JSON.parse(data)
-	if (ssr1.data.amount>0)   
-tz+='【阅读金币1】获得'+ssr1.data.amount+'金币\n'
-
-
-resolve()
-    })
+//下载脚本
+async function downFile() {
+    const url = 'https://raw.githubusercontent.com/ziye12/JavaScript/master/qqread.js'
+    await download(url, './', { filename: "temp.js" })
+    console.log("下载代码完毕");
 }
-   })
-  }  
 
-
-
-
-//阅读金币2
-function qqreadssr2() {
-return new Promise((resolve, reject) => {
-
-  const toqqreadssr2url = {url: `https://mqqapi.reader.qq.com/mqq/red_packet/user/read_time?seconds=300`,
-
-    headers: JSON.parse(qqreadheaderVal),
-   timeout:60000};
-
-if (config.data.pageParams.todayReadSeconds/60>=5){
-
-   $.get(toqqreadssr2url,(error, response, data) =>{
-     if(logs) $.log(`${jsname}, 金币奖励2: ${data}`)
-     ssr2 =JSON.parse(data)
-	if (ssr2.data.amount>0)   
-tz+='【阅读金币2】获得'+ssr2.data.amount+'金币\n'
-
-
-resolve()
-    })
+//替换内容
+async function changeFiele() {
+    let content = await fs.readFileSync("./temp.js", "utf8");
+    content = await smartReplace.replaceWithSecrets(content, Secrets);
+    //console.log(content);
+    await fs.writeFileSync("./execute.js", content, "utf8");
+    console.log("替换变量完毕");
 }
-   })
-  }  
 
-
-
-
-
-
-//阅读金币3
-function qqreadssr3() {
-return new Promise((resolve, reject) => {
-
-  const toqqreadssr3url = {url: `https://mqqapi.reader.qq.com/mqq/red_packet/user/read_time?seconds=1800`,
-
-    headers: JSON.parse(qqreadheaderVal),
-   timeout:60000};
-
-if (config.data.pageParams.todayReadSeconds/60>=30){
-
-   $.get(toqqreadssr3url,(error, response, data) =>{
-     if(logs) $.log(`${jsname}, 金币奖励3: ${data}`)
-     ssr3 =JSON.parse(data)
-	if (ssr3.data.amount>0)   
-tz+='【阅读金币3】获得'+ssr3.data.amount+'金币\n'
-
-
-resolve()
-    })
+async function deleteFile(path) {
+    // 查看文件result.txt是  否存在,如果存在,先删除
+    const fileExists = await fs.existsSync(path);
+    // console.log('fileExists', fileExists);
+    if (fileExists) {
+        const unlinkRes = await fs.unlinkSync(path);
+        // console.log('unlinkRes', unlinkRes)
+    }
 }
-   })
-  }  
 
-
-
-
-
-
-
-
-
-
-
-//金币签到
-function qqreadsign() {
-return new Promise((resolve, reject) => {
-
-  const toqqreadsignurl ={url: 'https://mqqapi.reader.qq.com/mqq/red_packet/user/clock_in/page',
-
-    headers: JSON.parse(qqreadheaderVal),
-   timeout:60000};
-   $.get(toqqreadsignurl,(error, response, data) =>{
-     if(logs) $.log(`${jsname}, 金币签到: ${data}`)
-     sign =JSON.parse(data)
-
-if (sign.data.videoDoneFlag)
- {
-tz+=
-'【金币签到】:获得'+sign.data.todayAmount+'金币\n'
+//启动
+async function start() {
+//console.log(` 当前执行时间:${new Date().toString()}`);
+console.log(`国际时间 (UTC+00)：${new Date().toLocaleString()}`)
+console.log(`北京时间 (UTC+08)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}\n`)
+    if (!Secrets.COOKIE_QQYD) {
+        console.log("请填写 Secrets 后再继续");
+        await notify.sendNotify(`${$.name}` + `${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}`, "请填写 Secrets");
+        return;
+    }
+    /*
+    if (!Secrets.SyncUrl) {
+        console.log("请填写 SYNCURL 后在继续");
+        return;
+    }
+    */
+    //console.log(`当前共${Secrets.Cookie.split("\n").length}个商品需要监测`);
+    try {
+        // 下载最新代码
+        await downFile();
+        // 替换变量
+        await changeFiele();
+        // 执行
+        await exec("node execute.js >> result.txt");
+    } catch (e) {
+        console.log("执行异常:" + e);
+    }
+    console.log("执行完毕");
+    const path = "./result.txt";
+    let content = "";
+    if (fs.existsSync(path)) {
+        content = fs.readFileSync(path, "utf8").replace(/[\r\n]+/g, `\r\n`)//去掉回车换行;
+        //console.log(content);
+    }
+    var d = new Date(new Date().getTime() + 8 * 60 * 60 * 1000);
+    //console.log(d.getHours() +'  '+ d.getMinutes());
+    if (content.includes("今日阅读") && d.getHours()==22 && d.getMinutes()<=20 ) {
+        await notify.sendNotify(`${$.name}` + `${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}`, content);
+        console.log(content)
+    } else {
+        //await notify.sendNotify(`${$.name}` + `${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}`, content);
+        console.log(content)
     }
 
-
-resolve()
-    })
-   })
-  }  
-
-
-
-
-//金币签到翻倍
-function qqreadsign2() {
-return new Promise((resolve, reject) => {
-
-  const toqqreadsign2url ={url: 'https://mqqapi.reader.qq.com/mqq/red_packet/user/clock_in_video',
-
-    headers: JSON.parse(qqreadheaderVal),
-   timeout:60000};
-   $.get(toqqreadsign2url,(error, response, data) =>{
-     if(logs) $.log(`${jsname}, 金币签到翻倍: ${data}`)
-     sign2 =JSON.parse(data)
-
-if (sign2.code==0)
- {
-tz+=
-'【签到翻倍】:获得'+sign2.data.amount+'金币\n'
-}
-
-
-
-
-resolve()
-    })
-   })
-  }  
-
-
-
-
-
-
-//每日阅读
-function qqreaddayread() {
-return new Promise((resolve, reject) => {
-
-  const toqqreaddayreadurl ={url: 'https://mqqapi.reader.qq.com/mqq/red_packet/user/read_book',
-
-    headers: JSON.parse(qqreadheaderVal),
-   timeout:60000};
-   $.get(toqqreaddayreadurl,(error, response, data) =>{
-     if(logs) $.log(`${jsname}, 每日阅读: ${data}`)
-     dayread =JSON.parse(data)
-
-if (dayread.code==0)
- {
-tz+=
-'【每日阅读】:获得'+dayread.data.amount+'金币\n'
+    //运行完成后，删除下载的文件
+    console.log('运行完成后，删除下载的文件\n')
+    await deleteFile(path);
 
 }
 
-
-resolve()
-    })
-   })
-  }  
-
-
-
-
-
-
-
-//视频奖励
-function qqreadvideo() {
-return new Promise((resolve, reject) => {
-
-  const toqqreadvideourl ={url: 'https://mqqapi.reader.qq.com/mqq/red_packet/user/watch_video',
-
-    headers: JSON.parse(qqreadheaderVal),
-   timeout:60000};
-   $.get(toqqreadvideourl,(error, response, data) =>{
-     if(logs) $.log(`${jsname}, 视频奖励: ${data}`)
-     video =JSON.parse(data)
-
-if (video.code==0)
- {
-tz+=
-'【视频奖励】:获得'+video.data.amount+'金币\n'
-
-}
-
-
-resolve()
-    })
-   })
-  }  
-
-
-
-
-
-
-
-//宝箱奖励
-function qqreadbox() {
-return new Promise((resolve, reject) => {
-
-  const toqqreadboxurl ={url: 'https://mqqapi.reader.qq.com/mqq/red_packet/user/treasure_box',
-
-    headers: JSON.parse(qqreadheaderVal),
-   timeout:60000};
-   $.get(toqqreadboxurl,(error, response, data) =>{
-     if(logs) $.log(`${jsname}, 宝箱奖励: ${data}`)
-     box =JSON.parse(data)
-
-if (box.data.count>=0)
- {
-tz+=
-'【宝箱奖励'+box.data.count+'】:获得'+box.data.amount+'金币\n'
-
- } 
-    
-
-
-resolve()
-    })
-   })
-  }  
-
-
-
-
-//宝箱奖励翻倍
-function qqreadbox2() {
-return new Promise((resolve, reject) => {
-
-  const toqqreadbox2url ={url: 'https://mqqapi.reader.qq.com/mqq/red_packet/user/treasure_box_video',
-
-    headers: JSON.parse(qqreadheaderVal),
-   timeout:60000};
-   $.get(toqqreadbox2url,(error, response, data) =>{
-     if(logs) $.log(`${jsname}, 宝箱奖励翻倍: ${data}`)
-     box2 =JSON.parse(data)
-
-if (box2.code==0)
- {
-tz+=
-'【宝箱翻倍】:获得'+box2.data.amount+'金币\n'
-}
-
-
-
-
-resolve()
-    })
-   })
-  }  
-
-
-
-
-
-
-//本周阅读时长
-function qqreadwktime() {
-return new Promise((resolve, reject) => {
-
-  const toqqreadwktimeurl = {
-
-    url: `https://mqqapi.reader.qq.com/mqq/v1/bookShelfInit`,
-
-    headers: JSON.parse(qqreadheaderVal),
-     
-    };
-
-   $.get(toqqreadwktimeurl,(error, response, data) =>{
-     if(logs) $.log(`${jsname}, 阅读时长: ${data}`)
-     wktime =JSON.parse(data)
-     if (wktime.code==0)
-tz+='【本周阅读时长】:'+wktime.data.readTime+'分钟\n'
-
-
-
-resolve()
-    })
-
-
-   })
-  }  
-
-
-
-
-
-
-
-
-//本周阅读时长奖励任务
-function qqreadpick() {
-return new Promise((resolve, reject) => {
-
-  const toqqreadpickurl = {
-
-    url:`https://mqqapi.reader.qq.com/mqq/pickPackageInit`,
-
-    headers: JSON.parse(qqreadheaderVal),
-     
-    };
-
-if (wktime.data.readTime>=wktimess){
-
-    $.get(toqqreadpickurl,(error, response, data) =>{
-     if(logs) $.log(`${jsname},周阅读时长奖励任务: ${data}`)
-     pick =JSON.parse(data)
-     if (pick.data[7].isPick==true)
-tz+='【周时长奖励】:已全部领取\n'
-
-
-for(let i=0;i<pick.data.length;i++)
- {
-	setTimeout(()=>{	 
-
-
-var pickid=pick.data[i].readTime
-
-var Packageid=['10','10','20','30','50','80','100','120']
-
- 
-const toqqreadPackageurl = {
-
-    url:`https://mqqapi.reader.qq.com/mqq/pickPackage?readTime=${pickid}`,
-
-    headers: JSON.parse(qqreadheaderVal),
-    
- timeout:60000};
-    $.get(toqqreadPackageurl,(error, response, data) =>{
-     if(logs) $.log(`${jsname}, 领周阅读时长: ${data}`)
-     Package =JSON.parse(data)
-     if (Package.code==0)
-tz+='【周时长奖励'+(i+1)+'】:领取'+Packageid[i]+'阅豆\n'
-
-     
-})			
-    
-
-
- },i*100)}
-
-
-
-})
-
-    resolve()
-
- }
-
-})
- }
-
-
-
-
-
-
-    
-
-function showmsg() {
-
-if (notifyInterval==1)
-$.msg(jsname,'',tz)//显示所有通知
-
-else if (notifyInterval==2&&box.data.amount>=0)
-$.msg(jsname,'',tz)//宝箱领取成功通知
-
-
-else if (notifyInterval==3&&box.data.count==0||box.data.count==18||box.data.count==36||box.data.count==54||box.data.count==72)
-$.msg(jsname,'',tz)//宝箱每18次通知一次
-
-
-
-}
-
+start()
 
 // prettier-ignore
 function Env(t,e){class s{constructor(t){this.env=t}send(t,e="GET"){t="string"==typeof t?{url:t}:t;let s=this.get;return"POST"===e&&(s=this.post),new Promise((e,i)=>{s.call(this,t,(t,s,r)=>{t?i(t):e(s)})})}get(t){return this.send.call(this.env,t)}post(t){return this.send.call(this.env,t,"POST")}}return new class{constructor(t,e){this.name=t,this.http=new s(this),this.data=null,this.dataFile="box.dat",this.logs=[],this.isMute=!1,this.isNeedRewrite=!1,this.logSeparator="\n",this.startTime=(new Date).getTime(),Object.assign(this,e),this.log("",`\ud83d\udd14${this.name}, \u5f00\u59cb!`)}isNode(){return"undefined"!=typeof module&&!!module.exports}isQuanX(){return"undefined"!=typeof $task}isSurge(){return"undefined"!=typeof $httpClient&&"undefined"==typeof $loon}isLoon(){return"undefined"!=typeof $loon}toObj(t,e=null){try{return JSON.parse(t)}catch{return e}}toStr(t,e=null){try{return JSON.stringify(t)}catch{return e}}getjson(t,e){let s=e;const i=this.getdata(t);if(i)try{s=JSON.parse(this.getdata(t))}catch{}return s}setjson(t,e){try{return this.setdata(JSON.stringify(t),e)}catch{return!1}}getScript(t){return new Promise(e=>{this.get({url:t},(t,s,i)=>e(i))})}runScript(t,e){return new Promise(s=>{let i=this.getdata("@chavy_boxjs_userCfgs.httpapi");i=i?i.replace(/\n/g,"").trim():i;let r=this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout");r=r?1*r:20,r=e&&e.timeout?e.timeout:r;const[o,h]=i.split("@"),a={url:`http://${h}/v1/scripting/evaluate`,body:{script_text:t,mock_type:"cron",timeout:r},headers:{"X-Key":o,Accept:"*/*"}};this.post(a,(t,e,i)=>s(i))}).catch(t=>this.logErr(t))}loaddata(){if(!this.isNode())return{};{this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e);if(!s&&!i)return{};{const i=s?t:e;try{return JSON.parse(this.fs.readFileSync(i))}catch(t){return{}}}}}writedata(){if(this.isNode()){this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e),r=JSON.stringify(this.data);s?this.fs.writeFileSync(t,r):i?this.fs.writeFileSync(e,r):this.fs.writeFileSync(t,r)}}lodash_get(t,e,s){const i=e.replace(/\[(\d+)\]/g,".$1").split(".");let r=t;for(const t of i)if(r=Object(r)[t],void 0===r)return s;return r}lodash_set(t,e,s){return Object(t)!==t?t:(Array.isArray(e)||(e=e.toString().match(/[^.[\]]+/g)||[]),e.slice(0,-1).reduce((t,s,i)=>Object(t[s])===t[s]?t[s]:t[s]=Math.abs(e[i+1])>>0==+e[i+1]?[]:{},t)[e[e.length-1]]=s,t)}getdata(t){let e=this.getval(t);if(/^@/.test(t)){const[,s,i]=/^@(.*?)\.(.*?)$/.exec(t),r=s?this.getval(s):"";if(r)try{const t=JSON.parse(r);e=t?this.lodash_get(t,i,""):e}catch(t){e=""}}return e}setdata(t,e){let s=!1;if(/^@/.test(e)){const[,i,r]=/^@(.*?)\.(.*?)$/.exec(e),o=this.getval(i),h=i?"null"===o?null:o||"{}":"{}";try{const e=JSON.parse(h);this.lodash_set(e,r,t),s=this.setval(JSON.stringify(e),i)}catch(e){const o={};this.lodash_set(o,r,t),s=this.setval(JSON.stringify(o),i)}}else s=this.setval(t,e);return s}getval(t){return this.isSurge()||this.isLoon()?$persistentStore.read(t):this.isQuanX()?$prefs.valueForKey(t):this.isNode()?(this.data=this.loaddata(),this.data[t]):this.data&&this.data[t]||null}setval(t,e){return this.isSurge()||this.isLoon()?$persistentStore.write(t,e):this.isQuanX()?$prefs.setValueForKey(t,e):this.isNode()?(this.data=this.loaddata(),this.data[e]=t,this.writedata(),!0):this.data&&this.data[e]||null}initGotEnv(t){this.got=this.got?this.got:require("got"),this.cktough=this.cktough?this.cktough:require("tough-cookie"),this.ckjar=this.ckjar?this.ckjar:new this.cktough.CookieJar,t&&(t.headers=t.headers?t.headers:{},void 0===t.headers.Cookie&&void 0===t.cookieJar&&(t.cookieJar=this.ckjar))}get(t,e=(()=>{})){t.headers&&(delete t.headers["Content-Type"],delete t.headers["Content-Length"]),this.isSurge()||this.isLoon()?(this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.get(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)})):this.isQuanX()?(this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t))):this.isNode()&&(this.initGotEnv(t),this.got(t).on("redirect",(t,e)=>{try{if(t.headers["set-cookie"]){const s=t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString();this.ckjar.setCookieSync(s,null),e.cookieJar=this.ckjar}}catch(t){this.logErr(t)}}).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>{const{message:s,response:i}=t;e(s,i,i&&i.body)}))}post(t,e=(()=>{})){if(t.body&&t.headers&&!t.headers["Content-Type"]&&(t.headers["Content-Type"]="application/x-www-form-urlencoded"),t.headers&&delete t.headers["Content-Length"],this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.post(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())t.method="POST",this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t));else if(this.isNode()){this.initGotEnv(t);const{url:s,...i}=t;this.got.post(s,i).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>{const{message:s,response:i}=t;e(s,i,i&&i.body)})}}time(t){let e={"M+":(new Date).getMonth()+1,"d+":(new Date).getDate(),"H+":(new Date).getHours(),"m+":(new Date).getMinutes(),"s+":(new Date).getSeconds(),"q+":Math.floor(((new Date).getMonth()+3)/3),S:(new Date).getMilliseconds()};/(y+)/.test(t)&&(t=t.replace(RegExp.$1,((new Date).getFullYear()+"").substr(4-RegExp.$1.length)));for(let s in e)new RegExp("("+s+")").test(t)&&(t=t.replace(RegExp.$1,1==RegExp.$1.length?e[s]:("00"+e[s]).substr((""+e[s]).length)));return t}msg(e=t,s="",i="",r){const o=t=>{if(!t)return t;if("string"==typeof t)return this.isLoon()?t:this.isQuanX()?{"open-url":t}:this.isSurge()?{url:t}:void 0;if("object"==typeof t){if(this.isLoon()){let e=t.openUrl||t.url||t["open-url"],s=t.mediaUrl||t["media-url"];return{openUrl:e,mediaUrl:s}}if(this.isQuanX()){let e=t["open-url"]||t.url||t.openUrl,s=t["media-url"]||t.mediaUrl;return{"open-url":e,"media-url":s}}if(this.isSurge()){let e=t.url||t.openUrl||t["open-url"];return{url:e}}}};this.isMute||(this.isSurge()||this.isLoon()?$notification.post(e,s,i,o(r)):this.isQuanX()&&$notify(e,s,i,o(r)));let h=["","==============\ud83d\udce3\u7cfb\u7edf\u901a\u77e5\ud83d\udce3=============="];h.push(e),s&&h.push(s),i&&h.push(i),console.log(h.join("\n")),this.logs=this.logs.concat(h)}log(...t){t.length>0&&(this.logs=[...this.logs,...t]),console.log(t.join(this.logSeparator))}logErr(t,e){const s=!this.isSurge()&&!this.isQuanX()&&!this.isLoon();s?this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t.stack):this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t)}wait(t){return new Promise(e=>setTimeout(e,t))}done(t={}){const e=(new Date).getTime(),s=(e-this.startTime)/1e3;this.log("",`\ud83d\udd14${this.name}, \u7ed3\u675f! \ud83d\udd5b ${s} \u79d2`),this.log(),(this.isSurge()||this.isQuanX()||this.isLoon())&&$done(t)}}(t,e)}
