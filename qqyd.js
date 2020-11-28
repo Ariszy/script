4a32ab88b7461503ccca13127eef6c86b4fe40ab
ZhiYi-N <34622776+ZhiYi-N@users.noreply.github.com>
2020-11-28 16:34:26 +0800

    Update qqyd.js

--- qqyd.js
+++ qqyd.js
@@ -87,7 +87,7 @@ console.log(`北京时间 (UTC+08)：${new Date(new Date().getTime() + 8 * 60 * 
     }*/
 
      if(SEND_KEY) {
-        if (content.includes("Cookie") && d.getHours()==16 && d.getMinutes()<=40) {
+        if (content.includes("Cookie") && d.getHours()==16 && d.getMinutes()<=30) {
             await notify.sendNotify("企鹅阅读-" + new Date().toLocaleDateString(), content);
             console.log("企鹅阅读-" + content)
         }else{
