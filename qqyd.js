6f672e64a46ec7033ad54b137cd456c217e734e6
ZhiYi-N <34622776+ZhiYi-N@users.noreply.github.com>
2020-11-29 17:26:41 +0800

    Update qqyd.js

--- qqyd.js
+++ qqyd.js
@@ -78,7 +78,7 @@ console.log(`北京时间 (UTC+08)：${new Date(new Date().getTime() + 8 * 60 * 
     }
     var d = new Date(new Date().getTime() + 8 * 60 * 60 * 1000);
     //console.log(d.getHours() +'  '+ d.getMinutes());
-    if (content.includes("今日阅读") && d.getHours()==12 && d.getMinutes()<=20 ) {
+    if (content.includes("今日阅读") ) {
         await notify.sendNotify(`${$.name}` + `${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}`, content);
         console.log(content)
     } else {
