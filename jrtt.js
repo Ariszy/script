9f9ce4cf4e99882ab8ff10620f5de64c986d5c9a
ZhiYi-N <34622776+ZhiYi-N@users.noreply.github.com>
2020-12-23 00:24:25 +0800

    Update jrtt.js

--- jrtt.js
+++ jrtt.js
@@ -218,7 +218,7 @@ if (!signurlArr[0]) {
       await control()
       //await sleepstart()
       //await sleepstop()
-      await collectcoins(2505)
+      await collectcoins(2517)
       await showmsg()
   }
  }
@@ -651,7 +651,7 @@ return new Promise((resolve, reject) => {
 function collectcoins(coins) {
 return new Promise((resolve, reject) => {
   let collectcoinsurl ={
-    url: `https://api3-normal-c-lq.snssdk.com/luckycat/lite/v1/sleep/done_task/?_request_from=web&device_platform=undefined&${signurl}`,
+    url: `https://api3-normal-c-lq.snssdk.com/luckycat/lite/v1/sleep/done_task/?_request_from=web&device_platform=undefined&ac=4G&${signurl}`,
     headers :JSON.parse(farmkey),
       timeout: 60000,
     body :JSON.stringify({score_amount: coins}),
