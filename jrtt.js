1542f844cef69199a2e1f0226575aaa23871f468
ZhiYi-N <34622776+ZhiYi-N@users.noreply.github.com>
2020-12-29 16:36:00 +0800

    Update jrtt.js

--- jrtt.js
+++ jrtt.js
@@ -650,8 +650,8 @@ return new Promise((resolve, reject) => {
 function collectcoins(coins) {
 return new Promise((resolve, reject) => {
   let collectcoinsurl ={
-    url: `https://api3-normal-c-lq.snssdk.com/luckycat/lite/v1/sleep/done_task/?_request_from=web&device_platform=undefined&${sigurl}`,
-    headers :JSON.parse(farmkey),
+    url: `https://api3-normal-c-lq.snssdk.com/luckycat/lite/v1/sleep/done_task/?_request_from=web&device_platform=undefined&${signurl}`,
+    headers :JSON.parse(farkey),
       timeout: 60000,
     body :JSON.stringify({score_amount: coins}),
 
