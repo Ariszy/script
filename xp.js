7b96194397ca81095440c07e3f96dcf827cf7dd1
ZhiYi-N <34622776+ZhiYi-N@users.noreply.github.com>
2021-01-11 14:46:07 +0800

    Update xp.js

--- xp.js
+++ xp.js
@@ -170,10 +170,12 @@ return new Promise((resolve, reject) => {
 async function showmsg(){
 if(tz==1){
     if ($.isNode()){
+     $.log(message)
     if ((hour == 12 && minute <= 20) || (hour == 23 && minute >= 40)) {
        await notify.sendNotify($.name,message)
      }
    }else{
+      $.log(message)
     if ((hour == 12 && minute <= 20) || (hour == 23 && minute >= 40)) {
        $.msg(jsname,'',message)
 }
